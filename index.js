2852b5f9bb2af86fe846bf33d5c6dae128ebf2ed
Renato Barbosa <tenat.renato@gmail.com>
2023-12-30 03:09:28 -0300

    Add slider

--- index.js
+++ index.js
@@ -1,20 +1,43 @@
 const menu = document.querySelector("#menu")
 const openButton = document.querySelector("#hamburguerIcon")
-const closeButton = document.querySelector ("#close")
+const closeButton = document.querySelector("#close")
 const opacidade = document.querySelector("#opacidade")
 
-openButton.addEventListener ("click", function (){
+const bolinhas = document.querySelectorAll(".bolinha")
+const pessoas = document.querySelectorAll(".pessoas")
+
+console.log(bolinhas);
+
+openButton.addEventListener("click", function () {
     menu.classList.add("flex");
-    openButton.classList.add ("none");
+    openButton.classList.add("none");
     closeButton.classList.add("flex");
     opacidade.classList.add("flex");
 })
 
-closeButton.addEventListener ("click", function (){
+closeButton.addEventListener("click", function () {
     menu.classList.remove("flex");
-    openButton.classList.remove ("none");
+    openButton.classList.remove("none");
     closeButton.classList.remove("flex");
     opacidade.classList.remove("flex");
 })
 
+bolinhas.forEach((bolinha, index) => {
+    bolinha.addEventListener("click", function () {
+        for (let i = 0; i < bolinhas.length; i++) {
+            bolinhas[i].removeAttribute("id")
+            pessoas[i].style.display = "none"
+        }
+        this.id = "bolinhaSelecionada"
+        pessoas[index].style.display = "flex"
+    })
+
+})
+
+window.addEventListener("resize", function () {
+    if (window.innerWidth > 992) {
+        pessoas.forEach(pessoa => pessoa.removeAttribute("style")) 
+    }
+}
+)
 
